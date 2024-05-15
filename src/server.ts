73002a525b3cd00d2b03f1caf416ec564af565b1
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-05-15 16:22:34 -0300

    raw: zod type providor n validator init;

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import fastify from 'fastify';
 
+import {
+    serializerCompiler,
+    validatorCompiler
+} from "fastify-type-provider-zod";
+
 import { health } from "../routes/health";
 import { course } from "../routes/course";
 
 const app = fastify();
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.register(health);
-
 app.register(course);
+
+
+
 
 app.listen({ port: 3000 })
     .then( () => {
