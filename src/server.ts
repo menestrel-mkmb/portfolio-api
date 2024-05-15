5af60a484268e536cc7ee02ce9146e1a4afc8298
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-05-15 14:49:02 -0300

    raw: init TS code to validate tsx script;

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+interface Course {
+    id: number;
+    name: string;
+    provedor: string;
+    tags: string[];
+    category: string;
+    duration: number;
+    verify_url: string;
+}
+
+console.log("init");
