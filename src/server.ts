9e8632c087fe2b2672303a6b43614a9b6635245e
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-05-15 15:34:44 -0300

    feat: register course methods on app;

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify';
 
 import { health } from "../routes/health";
+import { course } from "../routes/course";
 
 const app = fastify();
 
 app.register(health);
+
+app.register(course);
 
 app.listen({ port: 3000 })
     .then( () => {
