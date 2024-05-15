5e2ef5aaed79e42f3f22c86befbfd0d866d79630
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-05-15 16:34:23 -0300

    misc: using zod TypeProvider w mockup data;

--- routes/course.ts
+++ routes/course.ts
@@ -1,22 +1,45 @@
-import { Course } from "@prisma/client";
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import {
+    FastifyInstance,
+    FastifyReply,
+    FastifyRequest
+} from "fastify";
+
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 const getCourses = async (request: FastifyRequest, reply: FastifyReply) => {
-    const courses = {
-        courses: [
-            { name: "course 1" },
-            { name: "course 2" },
-            { name: "course 3" },
-            { name: "course 4" },
-            { name: "course 5" },
-            { name: "course 6" }
-        ]
-    }
-    return { courses };
+    // const courses = getCoursesResponseSchema.parse([
+    const courses = ([
+        {
+            id: "123e4567-e89b-12d3-a456-426614174000",
+            name: "Course 1",
+            provedor: "Course 1",
+            category: "Course 1",
+            duration: 30,
+            verifyUrl: "https://example.com"
+        },
+        {
+            id: "123e4567-e89b-12d3-a456-426614174001",
+            name: "Course 2",
+            provedor: "Course 2",
+            category: "Course 2",
+            duration: 60,
+            verifyUrl: "https://example.com"
+        }
+    ]);
+    return reply.send(courses);
 }
 
 const postCourses = async (request: FastifyRequest, reply: FastifyReply) => {
-    const course = request.body as Course;
+    // const course = postCourseRequestSchema.parse(request.body);
+    const course = {
+        id: "123e4567-e89b-12d3-a456-426614174002",
+        name: "Course 3",
+        provedor: "Course 3",
+        category: "Course 3",
+        duration: 90,
+        verifyUrl: "https://example.com"
+    }
 
     console.log(course.name, course.provedor, course.category, course.duration, course.verifyUrl);
 
@@ -24,6 +47,12 @@ const postCourses = async (request: FastifyRequest, reply: FastifyReply) => {
 }
 
 export async function course(app: FastifyInstance) {
-    app.get("/courses", getCourses)
-        .post("/courses", postCourses)
+    app
+        .withTypeProvider<ZodTypeProvider>()
+        .get("/courses",
+            // {schema: getCoursesSchema},
+            getCourses)
+        .post("/courses",
+            // {schema: postCourseSchema},
+            postCourses)
 }
