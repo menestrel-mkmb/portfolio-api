c7e1f9d5837590cfd1f9e07ae750a42b61b5cb45
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-05-20 15:06:21 -0300

    misc: POST /works;WIP

--- src/routes/work.ts
+++ src/routes/work.ts
@@ -19,9 +19,13 @@ const workIdSchema = z.object({
 const workObjectSchema = z.object({
     name: z.string(),
     occupation: z.string(),
-    startDate: z.date(),
-    endDate: z.date().optional(),
+    // startDate: z.date(),
+    // endDate: z.date().optional(),
+    startDate: z.string(),
+    endDate: z.string().optional(),
     description: z.string(),
+    category: z.string(),
+    statement: z.string().optional(),
 });
 const workObjectSchemaWithId = workIdSchema.merge(workObjectSchema);
 
@@ -48,18 +52,79 @@ const getWorks = async (request: FastifyRequest, reply: FastifyReply) => {
     return reply.send(works);
 };
 
+const getWorkDetailsRequestSchema = workIdSchema;
+const getWorkDetailsResponseSchema = {
+    200: workObjectSchemaWithId
+};
+const getWorkDetailsSchema = {
+    summary: "Get work details",
+    tags: ["works", "details"],
+    params: getWorkDetailsRequestSchema,
+    response: getWorkDetailsResponseSchema
+};
+
+const getWorkDetails = async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id } = getWorkDetailsRequestSchema.parse(request.params);
+    const workExists = await prisma.work.findUnique({
+        where: {
+            id
+        }
+    });
+
+    if(!workExists) throw new Error("Work not found");
+
+    return reply.send(workExists);
+}
+
+const postWorkRequestSchema = workObjectSchema;
+const postWorkResponseSchema = {
+    201: workObjectSchemaWithId
+}
+const postWorkSchema = {
+    summary: "Create new work",
+    tags: ["works"],
+    body: postWorkRequestSchema,
+    response: postWorkResponseSchema
+};
+
+const postWork = async (request: FastifyRequest, reply: FastifyReply) => {
+    const work = postWorkRequestSchema.parse(request.body);
+
+    const workExists = await prisma.work.findUnique({
+        where: {
+            name: work.name
+        }
+    });
+
+    if(workExists) throw new Error("Work name already exists");
+
+    console.log(work);
+
+    // const prismaWork = await prisma.work.create({
+    //     data: work
+    // });
+
+    return reply
+            .status(201)
+            .send({
+                "id": "00000000-0000-0000-0000-000000000000",
+                ...work
+            });
+            // .send(prismaWork);
+}
+
 export async function work(app: FastifyInstance) {
     app
         .withTypeProvider<ZodTypeProvider>()
         .get("/works",
             {schema: getWorksSchema},
             getWorks)
-        // .get("/works/:id",
-        //     {schema: getWorkDetailsSchema},
-        //     getWorkDetails)
-        // .post("/works",
-        //     {schema: postWorkSchema},
-        //     postWork)
+        .get("/works/:id",
+            {schema: getWorkDetailsSchema},
+            getWorkDetails)
+        .post("/works",
+            {schema: postWorkSchema},
+            postWork)
         // .patch("/works/:id",
         //     {schema: patchWorkSchema},
         //     patchWork)
