3e339ac28cbb13ce3608b77ba54999731d0b6e79
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-05-21 16:34:19 -0300

    misc: GET /education/:id;WIP

--- src/routes/education.ts
+++ src/routes/education.ts
@@ -50,10 +50,34 @@ const getAllEducation = async (request: FastifyRequest, reply: FastifyReply) => 
     reply.send(education);
 }
 
+export const getEducationDetailsResponseSchema = {
+    200: educationObjectSchemaWithId
+};
+export const getEducationDetailsSchema = {
+    summary: "Get education details",
+    tags: ["education"],
+    params: educationIdSchema,
+    response: getEducationDetailsResponseSchema
+};
+
+const getEducationDetails = async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id } = educationIdSchema.parse(request.params);
+    const prismaEducation = await prisma.education.findUnique({ where: { id } });
+
+    if(!prismaEducation) throw new Error("Education not found");
+
+    const education = (getEducationDetailsResponseSchema[200]).parse(prismaEducation);
+
+    reply.send(education);
+}
+
 export async function education(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .get("/education",
         { schema: getAllEducationSchema },
-        getAllEducation);
+        getAllEducation)
+    .get("/education/:id",
+        { schema: getEducationDetailsSchema },
+        getEducationDetails)
 }
