a8515d58be2d847addc5aabfe2640982ae28aa62
Michael <56491958+menestrel-mkmb@users.noreply.github.com>
2024-10-09 14:03:56 -0300

    fix(error): ZodError on errorHandler;

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -2,15 +2,17 @@ import { FastifyInstance } from 'fastify';
 import { BadRequestError } from './errors/bad-request';
 import { DuplicateEntityError } from './errors/duplicate-entity';
 import { NotFoundError } from './errors/not-found';
+import { ZodError } from 'zod';
 
 type FastifyErrorHandler = FastifyInstance['errorHandler'];
 
 export const errorHandler: FastifyErrorHandler = async (error, request, reply) => {
     request.log.error(error);
 
-    console.log('constructor', error.constructor);
+    // console.log('constructor', error.constructor);
     let errorCode = null;
     switch(error.constructor){
+        case ZodError:
         case BadRequestError:
             errorCode = 400;
             break;
